5b069b5ba3aee0fdd57a86b8b99671056e1ee546
Hamza Kayıcı <hamzakayc@gmail.com>
2025-12-31 16:40:40 +0300

    feat(web): add hour support to player timer

--- packages/web/src/pages/player/Player.tsx
+++ packages/web/src/pages/player/Player.tsx
@@ -258,8 +258,13 @@ const Player = () => {
     };
 
     const formatTime = (time: number) => {
-        const minutes = Math.floor(time / 60);
+        const hours = Math.floor(time / 3600);
+        const minutes = Math.floor((time % 3600) / 60);
         const seconds = Math.floor(time % 60);
+
+        if (hours > 0) {
+            return `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        }
         return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
     };
 
