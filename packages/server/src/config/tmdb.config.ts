6602d07e2adefaa7124a1c6baeaed3ec8b8a4fbd
Hamza Kayıcı <hamzakayc@gmail.com>
2025-12-31 11:45:28 +0300

    fix(tmdb): update read token and allow env injection

--- packages/server/src/config/tmdb.config.ts
+++ packages/server/src/config/tmdb.config.ts
@@ -1,5 +1,5 @@
 export const tmdbConfig = {
     apiKey: process.env.TMDB_API_KEY || '120b654264f473c26e07e34077ab9cdf',
-    readToken: process.env.TMDB_Read_Token || 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjBiNjU0MjY0ZjQ3M2MyNmUwN2UzNDA3N2FiOWNkZiIsInN1YiI6IjY2MTU4NzkwMjU2OS41ODMwMDAyLCJzY29wZXMiOlsiYXBpX3JlYWRdLCJ2ZXJzaW9uIjoxfQ.kvu5w_UrPxpbHByMwvdu6JQ2EVuoGSkU3-EGIp9-r1c',
+    readToken: process.env.TMDB_Read_Token || 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjBiNjU0MjY0ZjQ3M2MyNmUwN2UzNDA3N2FiOWNkZiIsIm5iZiI6MTU4NzkwMjU2OS41ODMwMDAyLCJzdWIiOiI1ZWE1Nzg2OTY2ZjJkMjAwMjQzZjQ1YjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kvu5w_UrPxpbHByMwvdu6JQ2EVuoGSkU3-EGIp9-r1c',
     apiUrl: 'https://api.themoviedb.org/3'
 };
